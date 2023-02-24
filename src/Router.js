347529303367cf822a2f1644369ad81b33410ac7
김한솔 <gimhansol@gimhansol-ui-MacBookAir.local>
2023-02-24 19:02:55 +0900

    Initialize: 피드백 반영

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Main from './pages/Main/Main';
+import Signup from './pages/Signup/Signup';
+import Login from './pages/Login/Login';
+import Detail from './pages/Detail/Detail';
+import Host from './pages/Host/Host';
+import Payment from './pages/Payment/Payment';
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
 
 function Router() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/host" element={<Host />} />
+        <Route path="/payment" element={<Payment />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
