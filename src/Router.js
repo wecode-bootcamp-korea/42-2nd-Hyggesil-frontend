30bc49d2ad56ca240be164366743bfd61fc0b3ba
seungchannn <121918663+seungchannn@users.noreply.github.com>
2023-02-28 16:22:51 +0900

    Modify : 리뷰반영

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
-import Signup from './pages/Signup/Signup';
+// import Signup from './pages/Signup/Signup';
 import Login from './pages/Login/Login';
 import Detail from './pages/Detail/Detail';
 import Host from './pages/Host/Host';
 import Payment from './pages/Payment/Payment';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
+import User from './pages/Login/components/User';
+import KakaoLogin from './pages/Login/components/KakaoLogin';
 
 function Router() {
   return (
@@ -15,11 +17,13 @@ function Router() {
       <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/hotels/:id" element={<Detail />} />
         <Route path="/host" element={<Host />} />
         <Route path="/payment" element={<Payment />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/kakaologin" element={<KakaoLogin />} />
       </Routes>
       <Footer />
     </BrowserRouter>
